9b49e9b5d2253afe6f3f3705d06d4c47c2d02541
Evgeny <eugen.eisner@gmail.com>
2023-02-27 19:44:53 +0100

    feat: add privacy

--- app/views.py
+++ app/views.py
@@ -5,23 +5,23 @@
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 from .forms import FormContact
+
+
 # from .models import Contact
 
 
 class IndexView(TemplateView, FormView):
-    template_name = 'app/index.html'
+    template_name = "app/index.html"
     form_class = FormContact
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy("home")
 
     def form_valid(self, form):
-        if self.request.POST.get('form_type') == 'form_1':
+        if self.request.POST.get("form_type") == "form_1":
             self.object = form.save(commit=False)
             self.object.save()
 
-        elif self.request.POST.get('form_type') == 'form_2':
+        elif self.request.POST.get("form_type") == "form_2":
             self.object = form.save(commit=False)
             self.object.save()
 
         return super().form_valid(form)
-
-
