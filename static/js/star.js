1a8dc88407234752ab0e38e3e0daeeb081fd2302
Evgeny Eysner <evgeny.eysner@gmail.com>
2024-04-01 20:12:41 +0200

    feat: add js for form

--- static/js/star.js
+++ static/js/star.js
@@ -2,6 +2,7 @@ const ratingItemsList = document.querySelectorAll('.rating__item')
 const ratingItemsArray = Array.prototype.slice.call(ratingItemsList)
 const starsCount = document.getElementById('star')
 const address = document.getElementById('address')
+const form = document.getElementById('contact-form')
 
 ratingItemsArray.forEach(item =>
     item.addEventListener('click', () => {
@@ -13,8 +14,7 @@ ratingItemsArray.forEach(item =>
     )
 )
 
-
-$('#contact-form').on('submit', function () {
+form.addEventListener('submit', () => {
     $.ajax({
         url: 'review/',
         type: 'POST',
@@ -47,6 +47,40 @@ $('#contact-form').on('submit', function () {
     })
     window.location.reload()
 })
+
+// $('#contact-form').on('submit', function () {
+//     $.ajax({
+//         url: 'review/',
+//         type: 'POST',
+//         headers: {
+//             'X-CSRFToken': '{{ csrf_token }}',
+//         },
+//
+//         data: {
+//             team: $('#team').val(),
+//             text: $('#review').val(),
+//             user: $('#username').val(),
+//             star: $('#star').val(),
+//         },
+//
+//         // handle a successful response
+//         success: function (json) {
+//             // remove the value from the input
+//             $('#team').val(''),
+//                 $('#star').val(''),
+//                 $('#review').val(''),
+//                 console.log(json); // log the returned json to the console
+//             console.log("success"); // another sanity check
+//         },
+//         // handle a non-successful response
+//         error: function (xhr, errmsg, err) {
+//             $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " + errmsg +
+//                 " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+//             console.log(xhr.status + ": " + xhr.responseText);
+//         }
+//     })
+//     window.location.reload()
+// })
 
 
 
